9df3b06e611ede444b7611a381aeeb214430f8a0
lyraxvincent <njongevincent@gmail.com>
2020-09-23 13:29:10 +0300

    some more changes

--- test.py
+++ test.py
@@ -1,24 +1,36 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
-import re
-#import numpy as np
+#import re
+import numpy as np
+import pandas as pd
 
-phonenames = ['samsung', 'huawei', 'nokia', 'tecno', 'infinix', 'apple', 'htc', 'google', 'xiaomi', 'motorola',
-              'blackberry', 'lg', 'oppo', 'sony', 'gionee', 'oneplus', 'cubot', 'hotwav', 'lenovo', 'vivo',
-              'lava', 'realme', 'honor', 'energizer', 'microsoft']
+phonesdata = {"phone title": [], "specs": [], "price": [], "rating": [], "specs score": [], "likes": []}
+site = "https://www.priceinkenya.com/phones/brand/lg"
+hdr = {'User-Agent': 'Mozilla/5.0'}
+req = Request(site, headers=hdr)
+page = urlopen(req)
+soup = BeautifulSoup(page, "html.parser")
 
-def getlinks(phonename):
-    links = []
-    site = "https://www.priceinkenya.com/phones/brand/{}".format(phonename)
-    links.append(site)  # first link
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = Request(site, headers=hdr)
-    page = urlopen(req)
-    soup = BeautifulSoup(page, "html.parser")
-    if soup.find("ul", {"class":"pagination pagination-sm my-3 justify-content-center"}):   # if there are other pages
-        tag_element = soup.find("ul", {"class":"pagination pagination-sm my-3 justify-content-center"})
-        for lnk in tag_element.findAll("a", {"href": re.compile("https://www.priceinkenya.com/phones/brand/{}*".format(phonename))}):
-            links.append(lnk["href"])
+for phone_tag in soup.findAll("div", {"class": "col-md-6 col-lg-4 fancy-border"}):
+    phone_tag = phone_tag.find("article")
+    phone_title = phone_tag.find("span", {"itemprop": "name", "class": False}).get_text()
+    phonesdata["phone title"].append(phone_title)
+    specs = phone_tag.find("div", {"class": "mb-4"}).get_text()
+    phonesdata["specs"].append(specs)
+    try:
+        price = phone_tag.find("span", {"class": "text-danger"}).get_text()
+        phonesdata["price"].append(price)
+    except:
+        price = np.nan
+        phonesdata["price"].append(price)
 
-    return links
+    rating = phone_tag.find("span", {"title": "Rating", "class": "mr-3"}).get_text()
+    phonesdata["rating"].append(rating)
+    specs_score = phone_tag.find("span", {"title": "Specs score"}).get_text()
+    phonesdata["specs score"].append(specs_score)
+    likes = phone_tag.find("div", {"class": "likes"}).find("like")[":likes-count"]
+    phonesdata["likes"].append(likes)
 
+
+df = pd.DataFrame(phonesdata)
+print(df.tail())
