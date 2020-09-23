010a45d458691aaa539d55ad1b69ea2558b267e3
lyraxvincent <njongevincent@gmail.com>
2020-09-23 22:24:27 +0300

    add folder for csv files

--- pricefromjumia.py
+++ pricefromjumia.py
@@ -3,9 +3,31 @@ from bs4 import BeautifulSoup
 import re
 import numpy as np
 import pandas as pd
+import string
 
-site = ""
-hdr = {'User-Agent': 'Mozilla/5.0'}
-req = Request(site,headers=hdr)
-page = urlopen(req)
-soup = BeautifulSoup(page, "html.parser")
+# read in csv file
+df = pd.read_csv("phonesdata.csv")
+df['Phone Title'] = df['Phone Title'].apply(lambda s: ''.join([l for l in s if l not in string.punctuation]))
+df['Phone Title'] = df['Phone Title'].apply(lambda s: ' '.join(s.split()[:-1]))
+df['Phone Title'] = df['Phone Title'].apply(lambda s: s.replace(" ", "+"))
+phonenames = list(df['Phone Title'])
+
+prices = []
+for i, phonename in enumerate(phonenames):
+    site = "https://www.jumia.co.ke/catalog/?q={}".format(phonename)
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = Request(site, headers=hdr)
+    page = urlopen(req)
+    soup = BeautifulSoup(page, "html.parser")
+
+    try:
+        price = soup.find("div", {"class": "prc"}).get_text()
+        prices.append(price)
+        print(f"{i}:Added price for [{phonename}]")
+    except:
+        price = np.nan
+        print(f"{i}:Price for [{phonename}] unavailable.")
+
+# add prices column to dataframe
+df['PriceJumia(Kshs)'] = pd.Series(prices)
+df.to_csv("phonesdata2.csv", index=False)
